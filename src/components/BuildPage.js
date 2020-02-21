1e856a758926ba52ee4e34814f8cdd66cd694e92
SeiryuuSuzakuu <seiryuusuzakuu@gmail.com>
2020-02-21 12:50:45 +0100

    fix memory leak

--- src/components/BuildPage.js
+++ src/components/BuildPage.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { getBuildsDataFromPath, getBuildsDataByDefinition } from '../api/buildsUrls';
-import { formatDistance } from 'date-fns';
+import React from "react";
+import {  getBuildsDataFromPath, getBuildsDataByDefinition } from "../api/buildsUrls";
+import { formatDistance } from "date-fns";
 
 class BuildPage extends React.Component {
   constructor() {
@@ -8,7 +8,7 @@ class BuildPage extends React.Component {
     this.state = {
       definitionsData: [],
       buildsData: [],
-      loading: false,
+      loading: false
     };
   }
 
@@ -20,11 +20,15 @@ class BuildPage extends React.Component {
           definition => definition.id
         );
 
-        getBuildsDataByDefinition(definitionIds.join(',')).then(data => {
+        getBuildsDataByDefinition(definitionIds.join(",")).then(data => {
           this.setState({ buildsData: data.value, loading: false });
         });
       });
     });
+  }
+
+  componentWillUnmount() {
+    this.setState({ definitionsData: [], buildsData: [], loading: false });
   }
 
   render() {
@@ -37,20 +41,22 @@ class BuildPage extends React.Component {
           <div
             style={{
               color:
-                build.status === 'completed'
-                  ? build.result === 'succeeded'
-                    ? 'green'
-                    : 'red'
-                  : 'blue',
+                build.status === "completed"
+                  ? build.result === "succeeded"
+                    ? "green"
+                    : "red"
+                  : "blue"
             }}
           >
             <a href={definitionLink}>{build.definition.name}</a>(
             <a href={build._links.web.href}>{build.buildNumber}</a>)
           </div>
-          <div>status: {build.status}{' '}
-            {build.status === 'completed' && formatDistance(new Date(), new Date(build.finishTime)) + ' ago'}
+          <div>
+            status: {build.status}{" "}
+            {build.status === "completed" &&
+              formatDistance(new Date(), new Date(build.finishTime)) + " ago"}
           </div>
-          {build.status === 'completed' && <div>result: {build.result}</div>}
+          {build.status === "completed" && <div>result: {build.result}</div>}
           <br />
         </div>
       );
